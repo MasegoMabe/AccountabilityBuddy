e92bbd92a4d4d8f85e44470752d9e48138e035e4
Masego Nontlantla Mirriam Mabe <180682611+MasegoMabe@users.noreply.github.com>
2026-04-01 20:53:23 +0200

    Initial Commit

---
+++ AccountabilityBuddy/AccountabilityBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountabilityBuddyApp.swift
+//  AccountabilityBuddy
+//
+//  Created by Masego Mabe on 2026. 04. 01..
+//
+
+import SwiftUI
+
+@main
+struct AccountabilityBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
